9ba5423c39e3cd96daab2ffe6aeb41311ce3fb45
nibekasov <nibekasov@gmail.com>
2020-12-11 01:07:03 +0300

    Update 05_35_BekasovNikita_buble_sort.R

--- 05_35_BekasovNikita_buble_sort.R
+++ 05_35_BekasovNikita_buble_sort.R
@@ -1,18 +1,18 @@
-#vektor<- c(1,2,3/5,-7)
-vektor<- c(3,4,6,2,1,5)
-counter<- 0 #variable that count amount of transpositions
+#vektor <- c(1,2,3/5,-7)
+vektor <- c(3,4,6,2,1,5)
+counter <- 0 #variable that count amount of transpositions
 
 for (i in 1:(length(vektor)-1)){
   print(0)
   print(vektor)
   for( j in 1:(length(vektor)-i)){
     print(1)
-    if (vektor[j]>vektor[j+1]){
+    if (vektor[j] > vektor[j+1]){
       
-      tmp<-vektor[j+1]
-      vektor[j+1]<- vektor[j]
-      vektor[j]<- tmp
-      counter<- counter + 1
+      tmp <-vektor[j+1]
+      vektor[j+1] <- vektor[j]
+      vektor[j] <- tmp
+      counter <- counter + 1
     }
   }
   
